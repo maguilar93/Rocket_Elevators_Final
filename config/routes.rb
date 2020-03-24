b8cbae975aaec742c237cb81c6a3e3e46de5f06f
RdaBouazzaoui <reda.bouazaoui10@gmail.com>
2020-03-24 14:03:38 -0400

    Fix geolocalisation index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  get 'geolocalisation/index'
+ 
   devise_for :users
   devise_for :employees, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :quote
   resources :leads
 
-  root to: 'geolocalisation#index'
+  # root to: 'geolocalisation#index'
   root 'leads#new'
 
   get 'index' => 'leads#new' # index
