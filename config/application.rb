e448f85d8b791a49b4f0128abdf45e4f876851fa
kirilkk9 <kirilkleinerman@gmail.com>
2020-03-25 13:07:47 -0400

    i hate merges

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 require 'faker'
-include 'IBMWatson'
+# include 'IBMWatson'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
