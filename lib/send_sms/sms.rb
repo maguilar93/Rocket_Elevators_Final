79dc5a358e134144e1d0636ffe2a2105c02157d2
maguilar93 <maguilarc@protonmail.com>
2020-03-26 17:38:37 -0400

    trying to change phone number to Ibrahim's

--- lib/send_sms/sms.rb
+++ lib/send_sms/sms.rb
@@ -11,7 +11,7 @@ module SendSms
             client = Twilio::REST::Client.new(account_sid, auth_token)
 
             from = ENV["TWILIO_PHONE_NUMBER"] # Your Twilio number
-            to = '+15149138302' # Your mobile phone number
+            to = '+14385057205' # Your mobile phone number
 
             client.messages.create(
             from: from,
