62cde768c1b834bad28ea2c01a4cda9f61d68402
afandyng <42415152+afandyng@users.noreply.github.com>
2020-03-27 16:03:03 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
 
   # SendGrid API
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "http://localhost:3000/" }
+  config.action_mailer.default_url_options = { host: "https://projectrocketelevators.com/" }
 end
 
 
