810b55ee2b8fe4a7419f5711f9ab04c3ba618232
root <root@LAPTOP-NB1TT34V.localdomain>
2020-04-17 13:12:41 -0400

    added recaptcha in sign up

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
-# Recaptcha.configure do |config|
-#   config.site_key   = Rails.application.config_for(:recaptcha_site_key)
-#   config.secret_key = Rails.application.config_for(:recaptcha_secret_key)
-# end
+Recaptcha.configure do |config|
+  config.site_key   = Rails.application.config_for(:application)
+  config.secret_key = Rails.application.config_for(:application)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "users/registrations"}
   devise_for :employees, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :pages
